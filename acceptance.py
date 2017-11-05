607106e3de07cce349cfea516708d027ddde098d
dragan.stepanovic <dragan.stepanovic@gmail.com>
2017-11-05 23:11:06 +0100

    method inlined

--- acceptance.py
+++ acceptance.py
@@ -5,15 +5,14 @@ import os
 class AcceptanceTests(unittest.TestCase):
     def test_reports_success_on_build_success(self):
         self.pull_code()
-        self.invoke_build()
+
+        ci_server = CiServer()
+        ci_server.run_build()
+
         self.assertTrue(self.get_result())
 
     def pull_code(self):
         self.assert_local_repo_exits()
-
-    def invoke_build(self):
-        ci_server = CiServer()
-        ci_server.start()
 
     def assert_local_repo_exits(self):
         if not os.path.isdir(r'C:\projects\pythonian\repo'):
@@ -31,9 +30,12 @@ class AcceptanceTests(unittest.TestCase):
 
 
 class CiServer:
-    def start(self):
-        pass
+    def run_build(self):
+        #repo folder must exist; cloned code doesn't
+        if not os.path.isdir(r'C:\projects\pythonian\repo'):
+            raise ValueError('repo folder does not exist')
 
+        
 
 if __name__ == '__main__':
     unittest.main()
